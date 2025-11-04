9ffed0ad639476813809a26f6e11dd68d57dc7a4
AliEY0 <ali.elyousfi@outlook.be>
2025-11-05 00:39:56 +0100

    scanf en printf in nasm

--- Book2011/Chapter9/reportNASM.asm
+++ Book2011/Chapter9/reportNASM.asm
@@ -1,21 +1,39 @@
 
 
-
+section .bss
+    num1 resd 1
 
 section .data
     msg1 db "Hoeveel users: ", 0
     frmS db "%s", 0
-
+    frmD db "%d", 0
+    nwln db 10, 0    
 section .text
     global main
     extern printf
-
+    extern scanf
 
 main:
     push msg1
     call printf
     add esp, 4
 
+    push num1
+    push frmD
+    call scanf
+    add esp, 8
+
+
+    push dword [num1]
+    push frmD
+    call printf
+    add esp, 8
+
+
+    push nwln 
+    call printf
+    
+    add esp, 4
 
     xor eax, eax
     ret
