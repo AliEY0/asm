be319b7001f5a182682ad52e0d84fecd567efec2
AliEY0 <ali.elyousfi@outlook.be>
2025-11-07 23:31:09 +0100

    ask string input and return count from string

--- Functions/String/StrLength/strlen.asm
+++ Functions/String/StrLength/strlen.asm
@@ -1,15 +1,22 @@
 
+section .bss
+    strIn resb 200 ;max lengte 200 char
 
 section .data
     strT db "Dit is een test string", 0
+    strInvoer db "Voer een string in: ", 0
+    frmS db "%s", 0
     strAns db "De lengte van uw string is: %d",10, 0
+    strGeeks db "%[^\n]%*c", 0
     nwln db 10, 0
 
 
 section .text
     global main
     extern printf
-
+    extern scanf
+    extern fgets
+    extern stdin
 
 slen:
     push ebp
@@ -32,20 +39,28 @@ fini:
 
 main:
     
-    push strT
-    call printf
-    add esp, 4
-    
-    push nwln
+    push strInvoer
     call printf
     add esp, 4
 
+    ;https://www.geeksforgeeks.org/c/taking-string-input-space-c-3-different-methods/    
+    push dword [stdin]
+    push dword 255
+    push dword strIn
+    call fgets
+    add esp, 12
 
+    push strIn
+    push frmS
+    call printf
+    add esp, 8
+        
 
-    push strT
+    push dword strIn
     call slen
     add esp, 4
-    
+
+    dec eax ;verwijdr nwline tel van teller
     push eax
     push strAns
     call printf
